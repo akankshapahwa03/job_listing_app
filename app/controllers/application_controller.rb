aca9edab194472548f934ca283ef315dc954f11b
Akanksha Pahwa <akanksha.pahwa@poplify.com>
2024-08-22 18:30:36 +0530

    Devise additions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  # after_action :flash_devise_errors, if: -> { devise_controller? && resource&.errors&.any? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: %i[role])
-    devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[name])
+    devise_parameter_sanitizer.permit(:invite, keys: %i[image name role])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[image name])
   end
 end
